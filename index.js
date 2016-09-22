fd7c368722e7d2827af6986c461c78bc576b8fa1
Jake Spencer <jake@ou.edu>
2016-09-22 08:13:05 -0500

    testing token

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+var citykey = process.env.CITY_SECRET_KEY;
+var citytoken = process.env.CITY_USER_TOKEN;
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World!\ntToken = ' + citytoken);
 });
 
 app.listen(port, function () {
